1f5efa7c3606716c470ecf0021110a043123b78a
MenassieErmias <ermiasmenase@gmail.com>
2024-11-05 15:13:35 +0300

    to be fixed:logo text appears below logo should appear next to logoat nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,26 @@
+'use client'
 
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href={'/'}>
+        <Image
+          src='/assets/images/logo.svg'
+          width={30}
+          height={30}
+          alt="Promptopia logo"
+          className="object-contain"
+        />
+        <p className="logo_text">
+          Promptopia
+        </p>
+      </Link>
+    </nav>
   )
 }
 
