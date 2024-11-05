93bcbd3477e41eb498d3f7fb0b952c3134b33ade
MenassieErmias <ermiasmenase@gmail.com>
2024-11-05 15:01:26 +0300

    hero secrtion content added: app/page.jsx, file structure for components added: @components/

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,22 @@
-
+import Feed from "@components/Feed"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col ">
+      <h1 className="head_text text-center">
+        Discover and share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">
+          &nbsp;AI powered prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dicta mollitia consequuntur voluptatibus distinctio, voluptate atque, autem quo debitis, minima necessitatibus illum optio temporibus incidunt nostrum vel? Dolorum, quis! Odio.
+      </p>
+
+      <Feed />
+
+    </section>
   )
 }
 
