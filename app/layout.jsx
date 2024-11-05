f79dca3159876d9c1f55fe4032852a5847dd1bff
MenassieErmias <ermiasmenase@gmail.com>
2024-11-05 14:52:29 +0300

    possible bug in globals.css: incorrect syntax setting inset, line 111, new layout.jsx and page.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover and share AI prompts'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <head>
+        <title>Promptopia</title>
+      </head>
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+        <main className='app'>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
